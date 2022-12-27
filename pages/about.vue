bda04721601a7464889b63bf69ade1540bb34d16
Al-gast <agastyo2004@gmail.com>
2022-12-27 17:56:18 +0700

    nuxt-app

---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit magni neque repudiandae, provident ipsa vel nesciunt, nihil, dolores voluptates id nisi ea. Reiciendis consequatur et porro, pariatur minus excepturi maiores.</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil, praesentium fugit repudiandae, facere eos illum vel ducimus iusto architecto, aut amet quaerat mollitia cumque et magni earum expedita accusantium numquam!</p>
+        <p>api response:</p>
+        <div>{{ data }}</div>
+    </div>
+</template>
+
+<script setup>
+    // const { data } = await useFetch('/api/ninja?name=mario')
+    // const { data } = await useFetch('/api/ninja?name=mario', {
+    //   method: 'post',
+    //   body: {age: 30}
+    // })
+
+    const { data } = await useFetch('/api/api')
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit magni neque repudiandae, provident ipsa vel nesciunt, nihil, dolores voluptates id nisi ea. Reiciendis consequatur et porro, pariatur minus excepturi maiores.</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil, praesentium fugit repudiandae, facere eos illum vel ducimus iusto architecto, aut amet quaerat mollitia cumque et magni earum expedita accusantium numquam!</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px;
+    }
+</style>
